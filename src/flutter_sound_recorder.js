1ac30f0347dc2a0fe93be18f4a8b7c8c79a4148b
Larpoux <larpoux@gmail.com>
2024-05-29 14:51:50 +0200

    TAU : Version 9.4.12

--- src/flutter_sound_recorder.js
+++ src/flutter_sound_recorder.js
@@ -16,7 +16,7 @@
  * along with Flutter-Sound.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const RECORDER_VERSION = '9.4.11'
+const RECORDER_VERSION = '9.4.12'
 
 const IS_RECORDER_PAUSED = 1;
 const IS_RECORDER_RECORDING = 2;
