dd3a14b8a8616288741489167329df5995e7dedb
Larpoux <larpoux@gmail.com>
2024-06-13 14:31:02 +0200

    TAU : Version 9.5.0

--- src/flutter_sound_player.js
+++ src/flutter_sound_player.js
@@ -16,7 +16,7 @@
  * along with Flutter-Sound.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const PLAYER_VERSION = '9.4.20'
+const PLAYER_VERSION = '9.5.0'
 
 function newPlayerInstance(aCallback, callbackTable) { return new FlutterSoundPlayer(aCallback, callbackTable); }
 
