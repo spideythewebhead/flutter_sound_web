af579f93f85735cede10aee65f76bbcb9cf58ad9
larpoux <larpoux@gmail.com>
2024-09-07 01:19:37 +0200

    TAU : Version 9.12.9

--- src/flutter_sound.js
+++ src/flutter_sound.js
@@ -16,7 +16,7 @@
  * along with Flutter-Sound.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const VERSION = '9.12.8'
+const VERSION = '9.12.9'
 
 
 const VERBOSE = 0;
--- src/flutter_sound_player.js
+++ src/flutter_sound_player.js
@@ -16,7 +16,7 @@
  * along with Flutter-Sound.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const PLAYER_VERSION = '9.12.8'
+const PLAYER_VERSION = '9.12.9'
 
 function newPlayerInstance(aCallback, callbackTable) { return new FlutterSoundPlayer(aCallback, callbackTable); }
 
