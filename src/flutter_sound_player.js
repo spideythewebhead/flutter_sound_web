217da28644f2511736d053277d68f64245e3e6d5
Pantelis Tsakoulis <spideythewebhead@gmail.com>
2024-11-03 21:42:16 +0200

    Set html5 as true when initializing holwer

--- src/flutter_sound_player.js
+++ src/flutter_sound_player.js
@@ -78,6 +78,7 @@ class FlutterSoundPlayer {
                         ({
                                 src: [path],
                                 format: tabFormat[codec],
+                                html5: true,
 
                                 onload: function () {
                                         me.callbackTable[CB_player_log](me.callback, DBG, 'onload');
